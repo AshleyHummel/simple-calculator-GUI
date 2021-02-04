f2653282f5ae33bcbf4244851be09ee0e4cde981
AshleyHummel <ashley.l.hummel@gmail.com>
2021-02-03 17:47:59 -0700

    Modified GUI
    Modified look and framework of GUI to be cross platform

--- CalculatorGUI/src/Calculator.java
+++ CalculatorGUI/src/Calculator.java
@@ -5,71 +5,110 @@
  * A simple java calculator GUI to practice Swing.
  */
 
-import java.awt.*;
-import javax.swing.*;
-import javax.swing.plaf.ColorUIResource;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Calculator extends JFrame 
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.UIManager;
+
+public class Calculator implements ActionListener
 {
+	//declare size constants
 	private static final int FRAME_WIDTH = 300;
-    private static final int FRAME_HEIGHT = 400;
+    private static final int FRAME_HEIGHT = 445;
     private static final int BUTTON_WIDTH = 10;
     private static final int BUTTON_HEIGHT = 15;
-    private Color baseColor;
-    private Color darkGrey;
-    private Color lightGrey;
-    private Color orange;
+    private static final int GAP = 5;
     
+    //initialize color and font
 	private Font font;
+	
+	private JFrame frame;
 	
 	private JTextField inputText;
 	private JButton btnBack, btnClear;
 	
-	public Calculator() {
+	public Calculator() 
+	{
+		try {
+		    UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+		 } catch (Exception e) {
+		            e.printStackTrace();
+		 }
+		
+		initGUI();
+	}
+	
+	private void initGUI() 
+	{
+		frame = new JFrame();
+		frame.setLayout(new BorderLayout());
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("Calculator");
+		frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
+		frame.setResizable(false);
+		
+		//initialize font and colors
 		font = new Font("Monospaced", Font.BOLD, 18);
-		baseColor = new ColorUIResource(46, 44, 41);
-		darkGrey = new ColorUIResource(69, 66, 63);
-		lightGrey = new ColorUIResource(110, 105, 99);
-		orange = new ColorUIResource(217, 150, 35);
 		
-		setLayout(new BorderLayout(10, 10));
-		getContentPane().setBackground(baseColor);
+		GridLayout topLayout = new GridLayout(2, 1);
+		topLayout.setHgap(GAP);
+		topLayout.setVgap(GAP);
+		JPanel topPanel = new JPanel(topLayout); //contains input text field AND "delete" buttons
 		
-		
-		JPanel topPanel = new JPanel(new GridLayout(2, 1));
-		configurePanel(topPanel);
-		
+		//set property of input textfield
 		inputText = new JTextField(20);
-		inputText.setForeground(Color.WHITE); //text color
-		inputText.setBackground(baseColor);
+		inputText.setForeground(Color.BLACK); //text color
+		inputText.setBackground(Color.WHITE);
 		inputText.setBorder(BorderFactory.createBevelBorder(0));
-		inputText.setPreferredSize(new Dimension(250, 60));
+		inputText.setPreferredSize(new Dimension(200, 75));
 		inputText.setHorizontalAlignment(SwingConstants.RIGHT);
 		inputText.setFont(new Font("Monospaced", Font.BOLD, 35)); //enlarge input text size
+		topPanel.add(inputText);
 		
-		JPanel deletePanel = new JPanel(new GridLayout(1, 2));
+		GridLayout delBtnsLayout = new GridLayout(1, 2);
+		delBtnsLayout.setHgap(GAP);
+		delBtnsLayout.setVgap(GAP);
+		JPanel deleteBtnsPanel = new JPanel(delBtnsLayout); //contains "delete" buttons
 		
+		//set property of "backspace" (←) button
 		btnBack = new JButton("←");
-		btnBack.setBackground(darkGrey);
 		configureButton(btnBack);
 		btnBack.setFont(new Font("Monospaced", Font.BOLD, 36)); //enlarge arrow
+		deleteBtnsPanel.add(btnBack);
 		
+		//set property of "clear" button
 		btnClear = new JButton("Clear");
-		btnClear.setBackground(darkGrey);
 		configureButton(btnClear);
+		frame.getContentPane().add(btnClear);
+		deleteBtnsPanel.add(btnClear);
 		
-		deletePanel.add(btnBack);
-		deletePanel.add(btnClear);
+		topPanel.add(deleteBtnsPanel);
 		
-		topPanel.add(inputText);
-		topPanel.add(deletePanel);
-
-		add(topPanel, BorderLayout.PAGE_START);
+		frame.getContentPane().add(topPanel, BorderLayout.PAGE_START);
 		
-		JPanel buttonPanel = new JPanel(new GridLayout(1, 2));
-		JPanel numberPanel = new JPanel(new GridLayout(4, 3));
-		configurePanel(numberPanel);
+		GridLayout btnsLayout = new GridLayout(1, 2);
+		btnsLayout.setHgap(GAP);
+		btnsLayout.setVgap(GAP);
+		JPanel btnsPanel = new JPanel(btnsLayout); //contains number AND operation buttons
+				
+		//initialize number panel
+		GridLayout numberLayout = new GridLayout(4, 3);
+		numberLayout.setHgap(GAP);
+		numberLayout.setVgap(GAP);
+		JPanel numberPanel = new JPanel(numberLayout);
 		
+		//define number button values
 		String[] btnValues  = {
 			"7", "8", "9",
 			"4", "5", "6",
@@ -77,25 +116,24 @@ public class Calculator extends JFrame
 			"0", ".", "="
 		};
 		
-		for(int i = 0; i < btnValues.length; i++) {
+		//configure number buttons using String array btnValues, and add to number panel
+		for(int i = 0; i < btnValues.length; i++) 
+		{
 			JButton btn = new JButton(btnValues[i]);
-			btn.setPreferredSize(new Dimension(BUTTON_WIDTH, BUTTON_HEIGHT));
-			
-			btn.setBackground(lightGrey);
-			if(i == btnValues.length-1) {
-				btn.setBackground(orange);
-			}
-			
-			btn.setOpaque(true);
 			configureButton(btn);
 			
 			numberPanel.add(btn);
-			
 		}
+		btnsPanel.add(numberPanel);
 		
-		JPanel operationsPanel = new JPanel(new GridLayout(4, 1));
-		configurePanel(operationsPanel);
+		//initialize operations panel
+		GridLayout operationsLayout = new GridLayout(4, 1);
+		operationsLayout.setHgap(GAP);
+		operationsLayout.setVgap(GAP);
+		JPanel operationsPanel = new JPanel(operationsLayout);
 		
+		
+		//define operation button values
 		String[] operationValues  = {
 				"÷",
 				"x",
@@ -103,43 +141,55 @@ public class Calculator extends JFrame
 				"+"
 			};
 		
-		for(int i = 0; i < operationValues.length; i++) {
+		//configure operation buttons using String array operationValues, and add to operations panel
+		for(int i = 0; i < operationValues.length; i++) 
+		{
 			JButton btn = new JButton(operationValues[i]);
-			btn.setPreferredSize(new Dimension(BUTTON_WIDTH-5, BUTTON_HEIGHT));
-			
-			btn.setBackground(orange);
 			configureButton(btn);
+			btn.setBorder(BorderFactory.createBevelBorder(0));
 			
 			operationsPanel.add(btn);
 		}
+		btnsPanel.add(operationsPanel);
 		
-		buttonPanel.add(numberPanel);
-		buttonPanel.add(operationsPanel);
+		JPanel content = new JPanel(new BorderLayout(5, 5));
+		content.add(topPanel, BorderLayout.NORTH);
+		content.add(btnsPanel);
 		
-		add(buttonPanel, BorderLayout.CENTER);
-
-		pack();
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setTitle("Calculator");
-		setSize(FRAME_WIDTH, FRAME_HEIGHT);
-		setVisible(true);
+		content.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
+		
+		frame.getContentPane().add(content, BorderLayout.CENTER);
+		
+		frame.setVisible(true);
 	}
 
-	private void configureButton(JButton btn) {
+	//configure default button settings
+	private void configureButton(JButton btn) 
+	{
 		btn.setFont(font);
 		btn.setBorder(BorderFactory.createBevelBorder(0));
-		btn.setForeground(Color.WHITE); //text color
+		btn.setForeground(Color.BLACK); //text color
+		btn.setPreferredSize(new Dimension(BUTTON_WIDTH, BUTTON_HEIGHT));
 		btn.setOpaque(true);
+		btn.addActionListener(this);
 		
 	}
 	
-	private void configurePanel(JPanel panel) {
-		panel.setBackground(darkGrey);
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
 	}
-	
+
 	public static void main(String[] args) 
 	{
 		new Calculator();
 	}
 
 }
+
+
+/* CHANGELOG
+ * 
+ * Modified the look of the GUI to be cross platform.
+ */
